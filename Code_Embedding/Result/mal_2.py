caa75728fcba3514ae5e3369d264711033908b3c
Ninjarku <33929885+Ninjarku@users.noreply.github.com>
2024-09-17 13:31:51 +0800

    Code that can embed python into another
    Created embed_code function to achieve code embedding with customizations, created helper functions with documentations to assist with this main function.

---
+++ Code_Embedding/Result/mal_2.py
@@ -0,0 +1,22 @@
+import random
+
+
+def injected_function():
+    print('This function was injected into the script.')
+    magic_number = random.randint(1, 200)
+    print(magic_number)
+    if magic_number > 50:
+        return 0
+    else:
+        return 1
+
+
+def main():
+    print('Hello world')
+    magic_number = random.randint(1, 200)
+    print(magic_number)
+
+
+if __name__ == '__main__':
+    if injected_function() == 1:
+        main()
---
+++ Code_Embedding/testcode.py
@@ -0,0 +1,11 @@
+import random
+
+
+def main():
+    print("Hello world")
+    magic_number = random.randint(1, 200)
+    print(magic_number)
+
+
+if __name__ == '__main__':
+    main()
